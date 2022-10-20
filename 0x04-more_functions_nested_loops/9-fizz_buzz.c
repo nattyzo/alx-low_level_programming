87f7cd67ce2cc70e03eaeba14116951616edb32e
nattyzo <nathnaelgebreselassie7@gmail.com>
2022-10-20 17:59:27 +0000

    Fixed error

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -18,15 +18,15 @@ int main(void)
 		}
 		putchar(' ');
 here:
-		if ((i % 3) == 0)
+		if ((i % 15) == 0)
 		{
-			printf("Fizz");
+			printf("FizzBuzz");
 		} else if ((i % 5) == 0)
 		{
 			printf("Buzz");
-		} else if (i % 15 == 0)
+		} else if (i % 3 == 0)
 		{
-			printf("FizzBuzz");
+			printf("Fizz");
 		} else
 		{
 			printf("%d", i);
