e67ccb51e4d9f633bb7d792d512e75385fa97d9b
nattyzo <nathnaelgebreselassie7@gmail.com>
2022-10-24 17:40:10 +0000

    Added Files

--- 0x05-pointers_arrays_strings/main.c
+++ 0x05-pointers_arrays_strings/main.c
@@ -1,17 +1,15 @@
-#include <stdio.h>
-
-void print_rev(char *);
+#include "main.h"
 
 /**
- *  * main - check the code for Holberton School students.
- *   *
- *    * Return: Always 0.
- *     */
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-		char *str;
+    char *str;
 
-			str = "Holberton!";
-				print_rev(str);
-					return (0);
+    str = "0123456789";
+    puts2(str);
+    return (0);
 }
