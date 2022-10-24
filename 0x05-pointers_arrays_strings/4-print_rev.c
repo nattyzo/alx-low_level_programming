e9e9209f28dfafb3ffe68341ffaf6352f88a89d9
Nathnael Asfaw <nathnaelgebreselassie7@gmail.com>
2022-10-24 10:56:07 +0000

    Added Files

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in a reverse order
+ * @s: the string to be printed
+ *
+ * Return: Nothing
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
