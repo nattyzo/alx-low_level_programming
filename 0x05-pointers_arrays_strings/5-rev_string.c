060cc311e541878b78cd161d4eb99f4309ca2705
Nathnael Asfaw <nathnaelgebreselassie7@gmail.com>
2022-10-26 12:33:57 +0000

    Added files

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * rev_string - is a function that reverses a string
+ *
+ * @s: is the variable that is going to be reversed
+ *
+ * Return: Nothing
+ */
+
+void rev_string(char *s)
+{
+
+	char i;
+
+	s = "My School"
+
+	for (s = 0; s != "\0"; s++)
+	{
+		_putchar(s);
+	}
+
+	 _putchar("\n");
+
+	 *s = i;
+
+	for (i = "\0"; i >= 0; i++)
+	{
+		_putchar(i);
+	}
+	_putchar(i);
+}
+
--- 0x05-pointers_arrays_strings/main.c
+++ 0x05-pointers_arrays_strings/main.c
@@ -2,29 +2,16 @@
 #include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
+ *  * main - check the code
+ *   *
+ *    * Return: Always 0.
+ *     */
 int main(void)
 {
-    int nb;
+	    char s[10] = "My School";
 
-    nb = _atoi("98");
-    printf("%d\n", nb);
-    nb = _atoi("-402");
-    printf("%d\n", nb);
-    nb = _atoi("          ------++++++-----+++++--98");
-    printf("%d\n", nb);
-    nb = _atoi("214748364");
-    printf("%d\n", nb);
-    nb = _atoi("0");
-    printf("%d\n", nb);
-    nb = _atoi("Suite 402");
-    printf("%d\n", nb);
-    nb = _atoi("         +      +    -    -98 Battery Street; San Francisco, CA 94111 - USA             ");
-    printf("%d\n", nb);
-    nb = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
-    printf("%d\n", nb);
-    return (0);
+	        printf("%s\n", s);
+		    rev_string(s);
+		        printf("%s\n", s);
+			    return (0);
 }
